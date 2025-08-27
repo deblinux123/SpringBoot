47f4b5c2164bd604bfdcab51199bd0b22adf3ddc
farid kaki <deltaburs@gmail.com>
2025-08-27 13:13:41 +0330

    componenet scanning

--- Chapter1/src/main/java/com/example/Chapter1/Chapter1Application.java
+++ Chapter1/src/main/java/com/example/Chapter1/Chapter1Application.java
@@ -3,6 +3,14 @@ package com.example.Chapter1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication
+//        (
+//                scanBasePackages = {
+//                        "com.example.Chapter1",
+//                        "com.example.util"
+//                }
+//        )
+
 @SpringBootApplication
 public class Chapter1Application {
 
--- Chapter1/src/main/java/com/example/Chapter1/common/Coach.java
+++ Chapter1/src/main/java/com/example/Chapter1/common/Coach.java
@@ -1,6 +1,17 @@
 package com.example.Chapter1.common;
 
+import org.springframework.stereotype.Component;
+
 public interface Coach
 {
     public String getDailyWorkout();
+
+    @Component
+    class CricketCoach implements Coach
+    {
+        @Override
+        public String getDailyWorkout() {
+            return "Practice fast 15 min";
+        }
+    }
 }
