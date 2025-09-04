c0db42d319a55835bc08d95b782a73f2fdd5f4c3
farid kaki <deltaburs@gmail.com>
2025-09-04 15:21:12 +0330

    updating using patch updating to update the specific part of the result

--- EmployeeDemoProject/src/main/java/com/example/EmployeeDemoProject/rest/EmployeeRestController.java
+++ EmployeeDemoProject/src/main/java/com/example/EmployeeDemoProject/rest/EmployeeRestController.java
@@ -4,10 +4,13 @@ package com.example.EmployeeDemoProject.rest;
 
 import com.example.EmployeeDemoProject.entity.Employee;
 import com.example.EmployeeDemoProject.service.EmployeeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -15,10 +18,13 @@ public class EmployeeRestController
 {
     private final EmployeeService employeeService;
 
+    private final ObjectMapper objectMapper;
+
     @Autowired
-    public EmployeeRestController(EmployeeService employeeService)
+    public EmployeeRestController(EmployeeService employeeService, ObjectMapper objectMapper)
     {
         this.employeeService = employeeService;
+        this.objectMapper = objectMapper;
     }
     @GetMapping("/employees")
     public List<Employee> findAll()
@@ -48,5 +54,35 @@ public class EmployeeRestController
     public Employee update(@RequestBody Employee employee)
     {
         return employeeService.save(employee);
+    }
+
+    @PatchMapping("/employees/{employeesId}")
+    public Employee patchUpdate(@PathVariable int employeesId, @RequestBody Map<String, Object> patchPayload)
+    {
+        Employee employee = employeeService.findById(employeesId);
+
+        if (employee == null)
+        {
+            throw new RuntimeException("Employee with id: " + employeesId + " not found");
+        }
+
+        if (patchPayload.containsKey("id"))
+        {
+            throw new RuntimeException("Employee id not allowed in request body -> " + employeesId);
+        }
+        Employee patchedEmployee = apply(patchPayload, employee);
+
+        return employeeService.save(patchedEmployee);
+    }
+
+    private Employee apply(Map<String, Object> patchPayload, Employee employee)
+    {
+        ObjectNode employeeNode = objectMapper.convertValue(employee, ObjectNode.class);
+
+        ObjectNode patchNode = objectMapper.convertValue(patchPayload, ObjectNode.class);
+
+        employeeNode.setAll(patchNode);
+
+        return objectMapper.convertValue(employeeNode, Employee.class);
     }
 }
