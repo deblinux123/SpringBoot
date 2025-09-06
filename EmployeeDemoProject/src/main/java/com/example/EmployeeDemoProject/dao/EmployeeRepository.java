d0c58fcc6acbcca663f7ce638f5b1366b5a41a26
farid kaki <deltaburs@gmail.com>
2025-09-06 12:21:24 +0330

    modifying using jpaRepository

--- EmployeeDemoProject/src/main/java/com/example/EmployeeDemoProject/dao/EmployeeRepository.java
+++ EmployeeDemoProject/src/main/java/com/example/EmployeeDemoProject/dao/EmployeeRepository.java
@@ -3,7 +3,6 @@ package com.example.EmployeeDemoProject.dao;
 import com.example.EmployeeDemoProject.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class EmployeeRepository extends JpaRepository<Employee, Integer>
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>
 {
-
 }
