ab42593706072205bd308a33dba1eba2bc69a324
farid kaki <deltaburs@gmail.com>
2025-09-06 13:46:16 +0330

    changing the end point with using repositoryrestresources annotation

--- EmployeeDemoProject/src/main/java/com/example/EmployeeDemoProject/dao/EmployeeRepository.java
+++ EmployeeDemoProject/src/main/java/com/example/EmployeeDemoProject/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.example.EmployeeDemoProject.dao;
 
 import com.example.EmployeeDemoProject.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>
 {
 }
