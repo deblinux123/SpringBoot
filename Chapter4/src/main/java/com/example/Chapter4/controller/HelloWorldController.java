db209d7e8008617490a0d6d16b7218638c0d89ba
farid kaki <deltaburs@gmail.com>
2025-09-08 12:25:59 +0330

    mvc controller

---
+++ Chapter4/src/main/java/com/example/Chapter4/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.Chapter4.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController
+{
+    @RequestMapping("/showform")
+    public String showForm()
+    {
+        return "showform";
+    }
+
+
+}
