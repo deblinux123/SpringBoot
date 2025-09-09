cc97ca8bba108fee0ced8bfd358a63827e388bee
farid kaki <deltaburs@gmail.com>
2025-09-09 16:18:35 +0330

    updating

---
+++ ValidationDemo/src/main/java/com/example/ValidationDemo/Customer.java
@@ -0,0 +1,20 @@
+package com.example.ValidationDemo;
+
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class Customer
+{
+    private String firstName;
+
+    @NotNull(message = "Is required")
+    @Size(min = 1, message = "Is Required")
+    private String lastName;
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+}
