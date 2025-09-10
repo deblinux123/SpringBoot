3d209e3ee08caa39fb1351cf3698ca65537db48a
farid kaki <deltaburs@gmail.com>
2025-09-10 11:41:14 +0330

    updating the customer class to validation min max

--- ValidationDemo/src/main/java/com/example/ValidationDemo/Customer.java
+++ ValidationDemo/src/main/java/com/example/ValidationDemo/Customer.java
@@ -1,6 +1,8 @@
 package com.example.ValidationDemo;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -12,9 +14,18 @@ public class Customer
     @Size(min = 1, message = "Is Required")
     private String lastName;
 
+    @Min(value= 0, message = "must be greater tha or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    private int freePasses;
+
+
     public String getFirstName() { return firstName; }
     public void setFirstName(String firstName) { this.firstName = firstName; }
 
     public String getLastName() { return lastName; }
     public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public int getFreePasses() { return freePasses; }
+    public void setFreePasses(int freePasses) { this.freePasses = freePasses; }
+
 }
