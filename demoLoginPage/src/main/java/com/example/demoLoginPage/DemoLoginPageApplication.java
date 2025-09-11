6186a11767219269ac2237565cdadb7e781b34b6
farid kaki <deltaburs@gmail.com>
2025-09-11 11:44:57 +0330

    delete application.properties and add the application.yaml

---
+++ demoLoginPage/src/main/java/com/example/demoLoginPage/DemoLoginPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoLoginPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLoginPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLoginPageApplication.class, args);
+	}
+
+}
---
+++ demoLoginPage/src/test/java/com/example/demoLoginPage/DemoLoginPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoLoginPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLoginPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
